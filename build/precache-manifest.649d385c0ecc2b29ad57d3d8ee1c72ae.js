f772d1819aca87c0908b8311c6229ffdf8d7d858
Lucas Jones <lucasejones@gmail.com>
2020-10-02 11:24:35 -0800

    added gitignore and fixed another error

---
+++ build/precache-manifest.649d385c0ecc2b29ad57d3d8ee1c72ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d953a625078f58b4bef0575d0d43956d",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f13bd03418ece4933249",
+    "url": "/Robofriends/static/css/main.4b85060b.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13bd03418ece4933249",
+    "url": "/Robofriends/static/js/main.eac471a1.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
